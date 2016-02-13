a1171a8bf8c8194304b6e803cb1526f6cf0f3474
kazyx <dev.ykz@gmail.com>
2016-02-13 21:48:38 +0900

    Remove Rfc6455ClientWebSocket

--- wirespider/core/src/main/java/net/kazyx/wirespider/rfc6455/Rfc6455.java
+++ wirespider/core/src/main/java/net/kazyx/wirespider/rfc6455/Rfc6455.java
@@ -10,6 +10,9 @@
 package net.kazyx.wirespider.rfc6455;
 
 import net.kazyx.wirespider.ClientWebSocket;
+import net.kazyx.wirespider.FrameRx;
+import net.kazyx.wirespider.FrameTx;
+import net.kazyx.wirespider.Handshake;
 import net.kazyx.wirespider.SelectorLoop;
 import net.kazyx.wirespider.SessionRequest;
 import net.kazyx.wirespider.WebSocketVersion;
@@ -19,6 +22,21 @@ import java.nio.channels.SocketChannel;
 public class Rfc6455 implements WebSocketVersion {
     @Override
     public ClientWebSocket newClientWebSocket(SessionRequest req, SelectorLoop loop, SocketChannel ch) {
-        return new Rfc6455ClientWebSocket(req, loop, ch);
+        return new ClientWebSocket(req, loop, ch) {
+            @Override
+            protected FrameTx newFrameTx() {
+                return new Rfc6455Tx(socketChannelProxy(), true);
+            }
+
+            @Override
+            protected FrameRx newFrameRx(FrameRx.Listener listener) {
+                return new Rfc6455Rx(listener, maxResponsePayloadSizeInBytes(), true);
+            }
+
+            @Override
+            protected Handshake newHandshake() {
+                return new Rfc6455Handshake(socketChannelProxy(), true);
+            }
+        };
     }
 }
